1ac04259d3bc7d2e86a487af1bc9e20c594e6b50
Alejandro Martinez <xcristopher335@gmail.com>
2022-07-07 18:56:30 -0400

    apikey

--- src/components/Settings/functions/fetchImages.js
+++ src/components/Settings/functions/fetchImages.js
@@ -2,7 +2,7 @@
 //563492ad6f9170000100000166684529eb26499ba5fe77c7eb8a6151
 //563492ad6f9170000100000103da485e3e614ecd9a308f4ae77ba519
 export default async function fetchData(amount,query) {
-     let apikey = "563492ad6f9170000100000166684529eb26499ba5fe77c7eb8a6151";
+     let apikey = "563492ad6f917000010000010d1e410d19224232b077571032f46ed1";
      
        
   let data = await fetch(
